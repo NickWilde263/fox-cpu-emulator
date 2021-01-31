dfdf5aa02310251bd5dc86b541bab6635882d9b8
Fox <67861857+NickWilde263@users.noreply.github.com>
2021-01-31 01:56:01 +0000

    Add files via upload

---
+++ util/base.h
@@ -0,0 +1,22 @@
+#ifndef _BASE_H
+#define _BASE_H
+
+
+#include <bits/stdc++.h>
+#include <cstddef>
+#include <cstdlib>
+#include <cstring>
+
+struct RGBA {
+  int R;
+  int G;
+  int B;
+  int A;
+};
+
+/* float operator "" f(char32_t n) {
+  return (float) n;
+} */
+
+float _fmod(float n1, float n2);
+#endif
